0d541033920537b8fe82eaf29637f89a4a154b6b
Shauk Wu <wushukainju@gmail.com>
2014-09-12 09:00:55 +0800

    abandom python tool

--- client/tools/buildscript/ChameleonTool/chaminfo.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class ChamInfo(object):
-    def __init__(self, prjmgr, mgr, env, localdir):
-        self.mgr = mgr
-        self.prjmgr = prjmgr
-        self.env = env
-        self.localdir = localdir
-
