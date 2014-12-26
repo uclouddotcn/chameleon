6e521f919d562704adab04aeb2953d3e2de155b4
Shauk Wu <wushukainju@gmail.com>
2014-12-26 10:34:56 +0800

    add stop support

--- server/lib/cluster/master.js
+++ server/lib/cluster/master.js
@@ -32,8 +32,16 @@ var Master = function () {
     cluster.setupMaster({
         exec: path.join(__dirname, 'worker.js')
     });
-    this.plugins = {};
+    this.workers = {};
 };
+
+Master.prototype.restartWorker = function (name, p, arg, logger) {
+    var self = this;
+    var stub = this.workers[name];
+    if (!worker) {
+        return null;
+    }
+}
 
 Master.prototype.startWorker = function (name, p, arg, logger) {
     var self = this;
@@ -53,6 +61,7 @@ Master.prototype.startWorker = function (name, p, arg, logger) {
         switch (msg.header.msg) {
             case '__start':
                 stub.__onStart(msg.body);
+                self.workers[name] = stub;
                 break;
             default:
                 stub.__onMsg(msg);
@@ -60,6 +69,50 @@ Master.prototype.startWorker = function (name, p, arg, logger) {
     });
     return stub;
 };
+
+Master.prototype._startWorker = function (name, p, arg, logger, stub) {
+    var self = this;
+    var worker = cluster.fork();
+    if (!stub) {
+        stub = new ClientStub(self, worker.id, logger);
+    }
+    worker.on('online', function () {
+        console.log('online')
+        worker.send({
+            header: {
+                msg: '__start'
+            },
+            p: p,
+            arg: arg
+        });
+    });
+    worker.on('message', function (msg) {
+        switch (msg.header.msg) {
+            case '__start':
+                stub.__onStart(msg.body);
+                self.workers[name] = stub;
+                break;
+            case '__onStop':
+                stub.__onStop(worker.id);
+                if (stub.isDone()) {
+                    self._removeWorker(stub);
+                    this.workers
+                }
+            default:
+                stub.__onMsg(msg);
+        }
+    });
+    return stub;
+};
+
+Master.prototype._removeWorker = function (stub) {
+    for (var i in this.workers) {
+        if (this.workers[i] == stub) {
+            delete this.workers[i];
+            break;
+        }
+    }
+}
 
 var master = new Master();
 
