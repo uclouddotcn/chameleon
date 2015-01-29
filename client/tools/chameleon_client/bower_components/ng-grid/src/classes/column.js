d104c9aa79647126259f58087b43926968471db9
Marco <Marco.liu@ucloud.cn>
2015-01-29 13:53:30 +0800

    Change: add bower modules.

---
+++ client/tools/chameleon_client/bower_components/ng-grid/src/classes/column.js
@@ -0,0 +1,98 @@
+﻿ng.Column = function (config, $scope, grid, domUtilityService) {
+    var self = this,
+        colDef = config.colDef,
+		delay = 500,
+        clicks = 0,
+        timer = null;
+    self.width = colDef.width;
+	self.groupIndex = 0;
+	self.isGroupedBy = false;
+    self.minWidth = !colDef.minWidth ? 50 : colDef.minWidth;
+    self.maxWidth = !colDef.maxWidth ? 9000 : colDef.maxWidth;
+    self.headerRowHeight = config.headerRowHeight;
+    self.displayName = colDef.displayName || colDef.field;
+    self.index = config.index;
+    self.isAggCol = config.isAggCol;
+    self.cellClass = colDef.cellClass;
+    self.cellFilter = colDef.cellFilter ? "|" + colDef.cellFilter : "";
+    self.field = colDef.field;
+    self.aggLabelFilter = colDef.cellFilter || colDef.aggLabelFilter;
+    self.visible = ng.utils.isNullOrUndefined(colDef.visible) || colDef.visible;
+    self.sortable = ng.utils.isNullOrUndefined(colDef.sortable) || colDef.sortable;
+    self.resizable = ng.utils.isNullOrUndefined(colDef.resizable) || colDef.resizable;
+    self.sortDirection = undefined;
+    self.sortingAlgorithm = colDef.sortFn;
+    self.headerClass = colDef.headerClass;
+    self.headerCellTemplate = colDef.headerCellTemplate || ng.defaultHeaderCellTemplate();
+    self.cellTemplate = colDef.cellTemplate || ng.defaultCellTemplate().replace(CUSTOM_FILTERS, self.cellFilter);
+    if (colDef.cellTemplate && !TEMPLATE_REGEXP.test(colDef.cellTemplate)) {
+        self.cellTemplate = ng.utils.getTemplatePromise(colDef.cellTemplate);
+    } 
+    if (colDef.headerCellTemplate && !TEMPLATE_REGEXP.test(colDef.headerCellTemplate)) {
+        self.headerCellTemplate = ng.utils.getTemplatePromise(colDef.headerCellTemplate);
+    }	
+	self.groupedByClass = function(){ 
+		return self.isGroupedBy ? "ngGroupedByIcon":"ngGroupIcon";
+	};
+    self.toggleVisible = function () {
+        self.visible = !self.visible;
+    };
+    self.showSortButtonUp = function () {
+        return self.sortable ? self.sortDirection === DESC : self.sortable;
+    };
+    self.showSortButtonDown = function () {
+        return self.sortable ? self.sortDirection === ASC : self.sortable;
+    };     
+    self.noSortVisible = function () {
+        return !self.sortDirection;
+    };
+    self.sort = function () {
+        if (!self.sortable) {
+            return; // column sorting is disabled, do nothing
+        }
+        var dir = self.sortDirection === ASC ? DESC : ASC;
+        self.sortDirection = dir;
+        config.sortCallback(self);
+    };   
+    self.gripClick = function () {
+        clicks++;  //count clicks
+        if (clicks === 1) {
+            timer = setTimeout(function () {
+                //Here you can add a single click action.
+                clicks = 0;  //after action performed, reset counter
+            }, delay);
+        } else {
+            clearTimeout(timer);  //prevent single-click action
+            config.resizeOnDataCallback(self);  //perform double-click action
+            clicks = 0;  //after action performed, reset counter
+        }
+    };
+    self.gripOnMouseDown = function (event) {
+        if (event.ctrlKey) {
+            self.toggleVisible();
+            domUtilityService.BuildStyles($scope, grid);
+            return true;
+        }
+        document.body.style.cursor = 'col-resize';
+        event.target.parentElement.style.cursor = 'col-resize';
+        self.startMousePosition = event.clientX;
+        self.origWidth = self.width;
+        $(document).mousemove(self.onMouseMove);
+        $(document).mouseup(self.gripOnMouseUp);
+        return false;
+    };
+    self.onMouseMove = function (event) {
+        var diff = event.clientX - self.startMousePosition;
+        var newWidth = diff + self.origWidth;
+        self.width = (newWidth < self.minWidth ? self.minWidth : (newWidth > self.maxWidth ? self.maxWidth : newWidth));
+        domUtilityService.BuildStyles($scope, grid);
+        return false;
+    };
+    self.gripOnMouseUp = function () {
+        $(document).off('mousemove');
+        $(document).off('mouseup');
+        document.body.style.cursor = 'default';
+        domUtilityService.apply($scope);
+        return false;
+    };
+};
---
+++ client/tools/chameleon_client/bower_components/ng-grid/workbench/main.js
@@ -0,0 +1,100 @@
+/// <reference path="../plugins/ng-grid-reorderable.js" />
+/// <reference path="../ng-grid-1.0.0.debug.js" />
+function userController($scope) {
+    var self = this;
+    $scope.mySelections = [];
+    $scope.mySelections2 = [];
+    $scope.myData = [];
+    $scope.filterOptions = {
+        filterText: "",
+        useExternalFilter: false,
+    };
+    $scope.pagingOptions = {
+        pageSizes: [250, 500, 1000], //page Sizes
+        pageSize: 250, //Size of Paging data
+        totalServerItems: 0, //how many items are on the server (for paging)
+        currentPage: 1 //what page they are currently on
+    };
+    self.getPagedDataAsync = function (pageSize, page, searchText) {
+        setTimeout(function () {
+            var data;
+            if (searchText) {
+                var ft = searchText.toLowerCase();
+                data = largeLoad().filter(function (item) {
+                    return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                });
+            } else {
+                data = largeLoad();
+            }
+            var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+            $scope.myData = pagedData;
+            $scope.pagingOptions.totalServerItems = data.length;
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        }, 100);
+    };
+    $scope.$watch('pagingOptions', function () {
+        self.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+    }, true);
+    $scope.$watch('filterOptions', function () {
+        self.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+    }, true);
+    self.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+    $scope.myDefs = [{ field: 'name', displayName: 'Very Long Name Title', sortable: false, headerClass: 'foo' },
+        { field: 'allowance', aggLabelFilter: 'currency', cellTemplate: 'partials/cellTemplate.html' },
+        { field: 'birthday', cellFilter: 'date', resizable: false },
+        { field: 'paid', cellFilter: 'checkmark' }];
+    $scope.myDefs2 = [{ field: 'Sku', displayName: 'My Sku' },
+        { field: 'Vendor', displayName: 'Supplier' },
+        { field: 'SeasonCode', displayName: 'My SeasonCode', cellTemplate: '<input style="width:100%;height:100%;" class="ui-widget input" type="text" ng-readonly="!row.selected" ng-model="row.entity[col.field]"/>' },
+        { field: 'Mfg_Id', displayName: 'Manufacturer ID' },
+        { field: 'UPC', displayName: 'Bar Code' }];
+    $scope.gridOptions = {
+		data: 'myData',
+		jqueryUITheme: false,
+		jqueryUIDraggable: false,
+        selectedItems: $scope.mySelections,
+        displaySelectionCheckbox: true,
+        multiSelect: true,
+        showGroupPanel: false,
+        showColumnMenu: true,
+        enablePaging: true,
+		sortInfo: {field: 'birthday', direction: 'asc'},
+		enableRowReordering: true,
+        maintainColumnRatios: false,
+        filterOptions: $scope.filterOptions,
+        pagingOptions: $scope.pagingOptions,
+        columnDefs: 'myDefs'
+    };
+    $scope.myData2 = [{ 'Sku': 'C-2820164', 'Vendor': 'NEWB', 'SeasonCode': null, 'Mfg_Id': '573-9880954', 'UPC': '822860449228' },
+                      { 'Sku': 'J-8555462', 'Vendor': 'NIKE', 'SeasonCode': '', 'Mfg_Id': '780-8855467', 'UPC': '043208523549' },
+                      { 'Sku': 'K-5312708', 'Vendor': 'REEB', 'SeasonCode': '1293', 'Mfg_Id': '355-6906843', 'UPC': '229487568922' },
+                      { 'Sku': 'W-4295255', 'Vendor': 'REEB', 'SeasonCode': '6283', 'Mfg_Id': '861-4929378', 'UPC': '644134774391' },
+                      { 'Sku': 'X-9829445', 'Vendor': 'DOCK', 'SeasonCode': '6670', 'Mfg_Id': '298-5235913', 'UPC': '872941679110' },
+                      { 'Sku': 'H-2415929', 'Vendor': 'REEB', 'SeasonCode': '3884', 'Mfg_Id': '615-8231520', 'UPC': '310547300561' },
+                      { 'Sku': 'X-2718366', 'Vendor': 'MERR', 'SeasonCode': '4054', 'Mfg_Id': '920-2961971', 'UPC': '157891269493' },
+                      { 'Sku': 'Q-1505237', 'Vendor': 'AX', 'SeasonCode': '9145', 'Mfg_Id': '371-6918101', 'UPC': '553657492213' },
+                      { 'Sku': 'M-1626429', 'Vendor': 'REEB', 'SeasonCode': '1846', 'Mfg_Id': '242-5856618', 'UPC': '029388467459' },
+                      { 'Sku': 'Y-1914652', 'Vendor': 'LEVI', 'SeasonCode': '5553', 'Mfg_Id': '80-9194110', 'UPC': '433360049369' }];
+    $scope.gridOptions2 = {
+        data: 'myData2',
+        selectedItems: $scope.mySelections2,
+        multiSelect: false,
+        enableRowReordering: true,
+        showGroupPanel: true,
+        columnDefs: 'myDefs2'
+    };
+    $scope.changeData = function () {
+        $scope.myDefs2 = undefined;
+    };
+    setTimeout(function () {
+        $scope.myDefs.splice(1, 1);
+        $scope.$apply();
+    }, 3000);
+    setTimeout(function () {
+        $scope.myData[0].name = "FOO";
+        $scope.$apply();
+    }, 5000);
+    
+};
