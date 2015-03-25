36bd27f4c97347cb1ad48c3fac5549164bbd7aa3
Marco <Marco.liu@ucloud.cn>
2015-03-25 11:46:28 +0800

    Change: fix a bug.

---
+++ client/tools/chameleon_client/js/constants.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2015/3/24.
+ */
+var path = require('path');
+
+function windowsHome(){
+    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+module.exports = {
+    chameleonHome: path.join(windowsHome(), '.chameleon')
+}
