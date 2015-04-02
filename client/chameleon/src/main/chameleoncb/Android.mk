fec723a34652362780ba540959e988a8bc3ea9e4
zhupeide <zpeide@gmail.com>
2015-04-02 13:39:19 +0800

    clean all the changes about lua

--- client/chameleon/src/main/chameleoncb/Android.mk
+++ client/chameleon/src/main/chameleoncb/Android.mk
@@ -1,12 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := chameleoncb 
-LOCAL_SRC_FILES := api_bind.cpp c_bind.cpp ChannelAPILib.cpp JniHelper.cpp ChannelAPILibC.cpp  
+LOCAL_MODULE := chameleoncb
+LOCAL_SRC_FILES := ChannelAPILib.cpp JniHelper.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_C_INCLUDES += /usr/local/include
-LOCAL_LDLIBS := $(LOCAL_PATH)/libluajit.a
-LOCAL_LDLIBS += -llog
-LOCAL_CFLAGS += -std=c++11 
-TARGET_PLATFORM := android-9
-include $(BUILD_SHARED_LIBRARY)
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_EXPORT_LDLIBS := -llog
+LOCAL_CFLAGS += -std=c++11
+include $(BUILD_STATIC_LIBRARY)
