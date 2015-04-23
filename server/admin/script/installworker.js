28733f7a64f3e29b069c2d86d245a8f651ac4f1c
Shauk Wu <wushukainju@gmail.com>
2015-04-23 15:25:18 +0800

    fix install bug

--- server/admin/script/installworker.js
+++ server/admin/script/installworker.js
@@ -12,7 +12,7 @@ function main(callback) {
     var workerzip = process.argv[2];
     var toInstallCfg = process.argv[3];
     var zipf = new AdmZip(workerzip);
-    var packageContent = JSON.parse(zipf.readAsText('worker/package.json'));
+    var packageContent = JSON.parse(zipf.readAsText('package.json'));
     var versionCode = toVersionCode(packageContent.version);
     var tmpFolder = path.join(workerDir, 'tmp-'+versionCode);
     var targetFolder = path.join(workerDir, versionCode.toString());
